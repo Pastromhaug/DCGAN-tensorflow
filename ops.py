cfe3d5187a27553081153be0b117329925773933
Per Andre Stromhaug <pas282@cornell.edu>
2017-01-01 17:21:58 -0500

    made tf.sqrt to np.sqrt

--- ops.py
+++ ops.py
@@ -61,7 +61,7 @@ def conv_cond_concat(x, y):
     return tf.concat(3, [x, y*tf.ones([x_shapes[0], x_shapes[1], x_shapes[2], y_shapes[3]])])
 
 def conv2d(input_, output_dim,
-           k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.02/tf.sqrt(2),
+           k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.02/np.sqrt(2),
            name="conv2d"):
     with tf.variable_scope(name):
         w = tf.get_variable('w', [k_h, k_w, input_.get_shape()[-1], output_dim],
@@ -74,7 +74,7 @@ def conv2d(input_, output_dim,
         return conv
 
 def deconv2d(input_, output_shape,
-             k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.02/tf.sqrt(2),
+             k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.02/np.sqrt(2),
              name="deconv2d", with_w=False):
     with tf.variable_scope(name):
         # filter : [height, width, output_channels, in_channels]
@@ -102,7 +102,7 @@ def deconv2d(input_, output_shape,
 def lrelu(x, leak=0.2, name="lrelu"):
   return tf.maximum(x, leak*x)
 
-def linear(input_, output_size, scope=None, stddev=0.02/tf.sqrt(2), bias_start=0.0, with_w=False):
+def linear(input_, output_size, scope=None, stddev=0.02/np.sqrt(2), bias_start=0.0, with_w=False):
     shape = input_.get_shape().as_list()
 
     with tf.variable_scope(scope or "Linear"):
